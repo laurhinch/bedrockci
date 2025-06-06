6a12b986a58d763c0e848e8a080c7cf188d2538b
Lauren Hinchcliffe <laurhinch@gmail.com>
2025-06-05 17:06:42 -0700

    Add initial application

---
+++ CONTRIBUTING.md
@@ -0,0 +1,38 @@
+# Contributing
+
+## Setup
+
+### Requirements
+
+- Rust (via [rustup](https://rustup.rs))
+- Git
+- Cargo
+- Ubuntu Linux
+
+### Build and Install
+
+```sh
+git clone https://github.com/laurhinch/bedrockci.git
+cd bedrockci
+cargo build
+cargo install --path cli
+```
+
+## Guidelines
+
+- Follow Rust best practices
+- Write tests for new features
+- Update docs for changes
+- Run tests before PRs
+
+## PR Process
+
+1. Fork repo
+2. Create feature branch
+3. Make changes
+4. Run tests
+5. Submit PR
+
+## License
+
+Contributions under MIT License 
---
+++ lib/src/lib.rs
@@ -0,0 +1,21 @@
+#[cfg(target_os = "linux")]
+#[cfg(not(target_os = "linux"))]
+compile_error!("This crate only supports Linux");
+
+#[cfg(target_os = "linux")]
+pub mod download;
+#[cfg(target_os = "linux")]
+pub mod server;
+#[cfg(target_os = "linux")]
+pub mod validate;
+
+#[cfg(target_os = "linux")]
+pub fn check_ubuntu() {
+    if let Ok(os_release) = std::fs::read_to_string("/etc/os-release") {
+        if !os_release.contains("Ubuntu") {
+            eprintln!(
+                "Warning: This crate has only been tested on Ubuntu. Other Linux distributions may not work as expected."
+            );
+        }
+    }
+}
