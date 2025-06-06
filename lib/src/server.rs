d1293ec7405af7a311bee28ed70f0689af6045e9
Lauren Hinchcliffe <laurhinch@gmail.com>
2025-06-05 17:15:24 -0700

    Fix various issues, resolve all warns

--- lib/src/server.rs
+++ lib/src/server.rs
@@ -1,9 +1,9 @@
+use crate::server_path::get_server_path;
 use anyhow::Result;
-use dirs::home_dir;
 
 /// Returns a list of downloaded server versions
 pub fn list_servers() -> Result<Vec<String>> {
-    let path = get_server_path();
+    let path = get_server_path()?;
 
     let versions = std::fs::read_dir(path)?
         .filter_map(|entry| entry.ok())
--- lib/src/server_path.rs
+++ lib/src/server_path.rs
@@ -1,12 +1,18 @@
+use dirs::home_dir;
+use std::path::PathBuf;
+
 /// Gets the server path, either the ENV variable BEDROCK_SERVER_PATH or the default path
-pub fn get_server_path() -> PathBuf {
-    let server_path = std::env::var("BEDROCK_SERVER_PATH").unwrap_or_else(|_| {
-        let home = home_dir().ok_or_else(|| {
-            anyhow::anyhow!(
-                "Could not find home directory, and no BEDROCK_SERVER_PATH environment variable set"
-            )
-        })?;
-        home.join(".bedrockci/server")
-    });
-    server_path
+pub fn get_server_path() -> Result<PathBuf, anyhow::Error> {
+    let server_path = match std::env::var("BEDROCK_SERVER_PATH") {
+        Ok(path) => PathBuf::from(path),
+        Err(_) => {
+            let home = home_dir().ok_or_else(|| {
+                anyhow::anyhow!(
+                    "Could not find home directory, and no BEDROCK_SERVER_PATH environment variable set"
+                )
+            })?;
+            home.join(".bedrockci/server")
+        }
+    };
+    Ok(server_path)
 }
